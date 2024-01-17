71389de83b5201de446c56d7069eeba74969d5d7
Murod Akhmedov <murod.london@gmail.com>
2024-01-17 18:48:15 +0000

    useState using

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import AlertFormView from './Views/AlertFormView';
 import ModifyNameView from './Views/ModifyNameView';
 
 export default function App() {
   return (
     <>
-      <AlertFormView />
       <ModifyNameView />
     </>
   );
--- src/Views/ModifyNameView.tsx
+++ src/Views/ModifyNameView.tsx
@@ -1,7 +1,27 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
-import { Alert, Button, Input } from '@chakra-ui/react';
+import {
+  Alert,
+  Badge,
+  Button,
+  Heading,
+  Input,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+
+const roles: Record<string, string | undefined> = {
+  Murod: 'owner',
+  David: 'teacher',
+};
 
 export default function ModifyNameView() {
+  const [listOfNames, setListOfNames] = useState<
+    string[]
+  >([]);
   const [name, setName] = useState('');
   const [previousName, setPreviousName] =
     useState<string>();
@@ -14,7 +34,38 @@ export default function ModifyNameView() {
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
     setPreviousName(name);
+    setListOfNames((current) => {
+      const newListOfNames = [...current, name];
+      return newListOfNames;
+    });
   }
+
+  const rows = listOfNames.map((name, index) => {
+    const role = roles[name];
+    const roleView = role != null && (
+      <Badge>{role}</Badge>
+    );
+    const getFirstCharacter = name[0];
+    const lastIndex = name.length - 1;
+    const lastCharacter = name.substring(lastIndex);
+    const firstCapital = getFirstCharacter.toUpperCase();
+    const restLower = name.substring(1).toLowerCase();
+    const capitalize = `${firstCapital}${restLower}`;
+    return (
+      <Tr key={index}>
+        <Td>
+          {name}
+          {roleView}
+        </Td>
+        <Td>{name.length}</Td>
+        <Td>{getFirstCharacter}</Td>
+        <Td>{lastCharacter}</Td>
+        <Td>{name.toLowerCase()}</Td>
+        <Td>{name.toUpperCase()}</Td>
+        <Td>{capitalize}</Td>
+      </Tr>
+    );
+  });
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -27,7 +78,22 @@ export default function ModifyNameView() {
       </form>
 
       <Button onClick={resetName}>Reset Name</Button>
-      <Alert>{previousName}</Alert>
+      <Alert> Last Submission: {previousName}</Alert>
+      <Heading>All Submissions</Heading>
+      <Table>
+        <Thead>
+          <Tr>
+            <Th>Submission</Th>
+            <Th>Number of Character</Th>
+            <Th>First Character</Th>
+            <Th>Last Character</Th>
+            <Th>Lower Case</Th>
+            <Th>Upper Case</Th>
+            <Th>Capitalize</Th>
+          </Tr>
+        </Thead>
+        <Tbody>{rows}</Tbody>
+      </Table>
     </>
   );
 }
